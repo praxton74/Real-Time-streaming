b748ce63add6f8f989636ef65b48a4fdf15f0d5a
Parth <94388365+praxton74@users.noreply.github.com>
2024-10-16 11:03:33 +0530

    producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,34 +1,24 @@
 from confluent_kafka import Producer
 import time
 
-
 def delivery_report(err, msg):
     if err is not None:
         print(f"Message delivery failed: {err}")
     else:
-        print(
-            f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}"
-        )
-
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
 
 def main():
     producer_config = {
-        "bootstrap.servers": "localhost:9092",
-    }
-
-    producer = Producer(producer_config)
-
-    topic = "social_media_stream"
-
+        "bootstrap.servers": "localhost:9092",}
+    producer=Producer(producer_config)
+    topic="social_media_stream"
     with open("sample_data.txt") as f:
         for line in f:
             producer.produce(
                 topic, value=line.strip().encode("utf-8"), callback=delivery_report
             )
             time.sleep(1)
-
     producer.flush()
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
