e77071782575f932bf23b9a47d66fc8b0246683c
Parth <94388365+praxton74@users.noreply.github.com>
2024-10-16 12:16:00 +0530

    kafka_consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -10,15 +10,14 @@ def main():
     topic="social_media_stream"
     consumer.subscribe([topic])
     try:
+        
         while True:
             message=consumer.poll(1.0)
-
             if message is None:
                 continue
             if message.error():
                 print(f"Consumer error: {message.error()}")
                 continue
-
             data=message.value().decode("utf-8")
             print(f"Received message: {data}")
             
