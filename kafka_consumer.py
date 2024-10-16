28fadf3e17fe7a5a17b41c23facb78797856def6
Parth <94388365+praxton74@users.noreply.github.com>
2024-10-16 11:02:44 +0530

    consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,20 +1,17 @@
 from confluent_kafka import Consumer
 
-
 def main():
-    consumer_config = {
+    consumer_config={
         "bootstrap.servers": "localhost:9092",
         "group.id": "my-group",
         "auto.offset.reset": "earliest",
     }
-
-    consumer = Consumer(consumer_config)
-    topic = "social_media_stream"
+    consumer=Consumer(consumer_config)
+    topic="social_media_stream"
     consumer.subscribe([topic])
-
     try:
         while True:
-            message = consumer.poll(1.0)
+            message=consumer.poll(1.0)
 
             if message is None:
                 continue
@@ -22,15 +19,13 @@ def main():
                 print(f"Consumer error: {message.error()}")
                 continue
 
-            data = message.value().decode("utf-8")
+            data=message.value().decode("utf-8")
             print(f"Received message: {data}")
-
+            
     except KeyboardInterrupt:
         pass
-
     finally:
         consumer.close()
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
